64f1d0fe1d40f9045d6fb5792d111b8bb4c8c5b7
Vladislav <vladislav0904@gmail.com>
2021-01-19 00:39:21 +0500

    Added ingame gui, quick bugfix

--- game_stage.py
+++ game_stage.py
@@ -10,31 +10,32 @@ def load_sound(filename):
 
 
 def stage_load(is_coop, number):
-    pygame.init()
-    width = 800
-    height = 600
-    FPS = 60
-    screen = pygame.display.set_mode((width, height))
-    clock = pygame.time.Clock()
-    pygame.display.set_caption('Battle City')
-    stage = load_sound('stage.wav')
-    screen.fill((128, 128, 128))
-    font = pygame.font.Font('./data/fonts/PixelEmulator-xq08.ttf', 48)
-    text = font.render(f"STAGE {number}", True, (255, 255, 255))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    screen.blit(text, (text_x, text_y))
-    stage.play()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                keys = pygame.key.get_pressed()
-                if keys[pygame.K_RETURN]:
-                    game_level.game(is_coop, number)
+    if number != 6:
+        pygame.init()
+        width = 800
+        height = 600
+        FPS = 60
+        screen = pygame.display.set_mode((width, height))
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Battle City')
+        stage = load_sound('stage.wav')
+        screen.fill((128, 128, 128))
+        font = pygame.font.Font('./data/fonts/PixelEmulator-xq08.ttf', 48)
+        text = font.render(f"STAGE {number}", True, (255, 255, 255))
+        text_x = width // 2 - text.get_width() // 2
+        text_y = height // 2 - text.get_height() // 2
+        screen.blit(text, (text_x, text_y))
+        stage.play()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    keys = pygame.key.get_pressed()
+                    if keys[pygame.K_RETURN]:
+                        game_level.game(is_coop, number)
 
+            pygame.display.flip()
+            clock.tick(FPS)
+        manager.update(FPS / 1000)
+        manager.draw_ui(screen)
         pygame.display.flip()
         clock.tick(FPS)
-    manager.update(FPS / 1000)
-    manager.draw_ui(screen)
-    pygame.display.flip()
-    clock.tick(FPS)
